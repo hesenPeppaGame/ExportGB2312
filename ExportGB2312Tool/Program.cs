7247e615a46f10ad0fae7dad59459b9aa8b8688b
PeppaHeSen <41465397+PeppaHeSen@users.noreply.github.com>
2020-12-05 11:04:06 +0800

    update
    代码更新

--- ExportGB2312Tool/Program.cs
+++ ExportGB2312Tool/Program.cs
@@ -13,17 +13,17 @@ namespace ExportGB2312Tool
     {
         static void Main(string[] args)
         {
-            WriteGB2312File2("D://gb2312.txt");
+            WriteGB2312File("D://gb2312.txt");
         }
 
         /// <summary>
         /// 输出GB2312所有字符
+        /// 字符未输出，只输出了汉字
         /// </summary>
         /// <param name="path"></param>
         public static void WriteGB2312File2(string path)
         {
             StringBuilder sb = new StringBuilder();
-            List<string> GBString = new List<string>();
             Encoding gb = Encoding.GetEncoding("gb2312");
             int count = 1;
             //输出所有ASCII
@@ -65,40 +65,37 @@ namespace ExportGB2312Tool
         private static void WriteGB2312File(string path)
         {
             StringBuilder sb = new StringBuilder();
-            List<string> GBString = new List<string>();
 
             //输出所有ASCII
             for (int i = 1; i <= 255; i++)
             {
                 char c = (char)i;//通过强制类型转换来将ASCII码对应的字符数出
-                GBString.Add(c.ToString());
+                sb.Append(c);
             }
 
             //输出所有GB2312
             byte[] by = new byte[2];
             int count = 0;
+            string source;
             Encoding gb = Encoding.GetEncoding("gb2312");
             for (int i = 109; i < 8894; i++)
             {
                 by[0] = (byte)(i / 100 + 160);
                 by[1] = (byte)(i % 100 + 160);
-                var source = gb.GetString(by);
-                GBString.Add(source);
+                source = gb.GetString(by);
+                sb.Append(source);
                 count++;
+                if (count % 40 == 0)
+                {
+                    sb.AppendLine();
+                }
+
                 if (i.ToString().EndsWith("94"))
                 {
                     i += 6;
                 }
             }
 
-            for (int i = 0; i < GBString.Count; i++)
-            {
-                sb.Append(GBString[i]);
-                if (i % 40 == 0)
-                {
-                    sb.Append("\r\n");
-                }
-            }
 
             using (StreamWriter sw = new StreamWriter(path))
             {
